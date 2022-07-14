d5c5bc32bcc7d92016ff4006b8fc209271c6ed17
Shivangi Gaur <75794258+shivangigaur01@users.noreply.github.com>
2022-07-14 22:29:16 +0530

    Update Z-function.java

--- Day-6 Strings Part-II/Z-function.java
+++ Day-6 Strings Part-II/Z-function.java
@@ -1,1 +1,18 @@
-h
+class Solution {
+    public int strStr(String h, String n) {
+       int l1 = h.length(), l2 = n.length();
+        if (l1 < l2) {
+            return -1;
+        } else if (l2 == 0) {
+            return 0;
+        }
+        int threshold = l1 - l2;
+        for (int i = 0; i <= threshold; ++i) {
+            if (h.substring(i,i+l2).equals(n)) {
+                return i;
+            }
+        }
+        return -1;
+    
+    }
+}
