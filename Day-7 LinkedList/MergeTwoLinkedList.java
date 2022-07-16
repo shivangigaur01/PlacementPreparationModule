62019eb5a3e343fc077bc5f30a9bfaccc439b88c
Shivangi Gaur <75794258+shivangigaur01@users.noreply.github.com>
2022-07-16 20:27:57 +0530

    Create MergeTwoLinkedList.java

---
+++ Day-7 LinkedList/MergeTwoLinkedList.java
@@ -0,0 +1,29 @@
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode dummy=new ListNode(0);
+        ListNode head=dummy;
+        while(list1!=null && list2!=null){
+            if(list1.val<list2.val){
+                dummy.next=list1;
+                list1=list1.next;
+                dummy=dummy.next;
+            }
+            else{
+                dummy.next=list2;
+                list2=list2.next;
+                dummy=dummy.next;
+            }
+        }
+        while(list1!=null){
+            dummy.next=list1;
+            list1=list1.next;
+            dummy=dummy.next;
+        }
+        while(list2!=null){
+            dummy.next=list2;
+            list2=list2.next;
+            dummy=dummy.next;
+        }
+        return head.next;
+    }
+}
